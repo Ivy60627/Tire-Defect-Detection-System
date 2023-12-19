11fef836815a37147f8cb4d570d1a079ed63eda9
Ivy60627 <40078512+Ivy60627@users.noreply.github.com>
2023-12-19 15:06:35 +0800

    1218 change reading function
    Change the function that can read six pictures and process multiple pictures

--- picture_connect.py
+++ picture_connect.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import numpy as np
 from PIL import Image
@@ -33,72 +34,85 @@ def resize_image(image, height=IMAGE_SIZE, width=IMAGE_SIZE):
      constant = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=BLACK)
      # 调整图像大小并返回     
      return cv2.resize(constant, (height, width))
-        
-img = cv2.imread('picture\\img_2_roi.png') 
-img2 = cv2.imread('picture\\img_2_roi.png')
 
-img = resize_image(img)
-img2 = resize_image(img2)
+path = './roi/'
+pic_list = []
+output_Image= []
+for root,dirs,files in os.walk(path):
+	for file in files:
+		file=file.rstrip(".png")
+		pic_list.append(file)
+
+pic_list = pic_list[:6]
+
+for index, img in enumerate(pic_list):        
+    img = cv2.imread('picture\\img_2_roi.png') 
+    #img2 = cv2.imread('picture\\img_2_roi.png')
+    
+    img = resize_image(img)
+    #img2 = resize_image(img2)
+         
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)  # 轉換成 BGRA 色彩模式
+    #img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGBA)  # 轉換成 BGRA 色彩模式  
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # 新增 gray 變數為轉換成灰階的圖片
+    
+    
+    
+    img = blackToClear(img)
+    #img2 = blackToClear(img2)
+    
+    # cv2.namedWindow('image22', cv2.WINDOW_KEEPRATIO)
+    # cv2.imshow('image22',img2)
+    # cv2.setMouseCallback('image22', show_xy)
+    bg = Image.new('RGBA',(8000, 8000), '#FFFFFF')
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    
+    
+    # # 1 
+    # output_Image = Image.fromarray(np.uint8(img))
+    # r,g,b,a = output_Image.split()
+    # bg.paste(output_Image,(1500, 1500),mask=a)
      
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)  # 轉換成 BGRA 色彩模式
-img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGBA)  # 轉換成 BGRA 色彩模式  
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # 新增 gray 變數為轉換成灰階的圖片
+    
+    # 2取得圖像的高度和寬度
+    (h, w) = img.shape[:2]
+    # 計算圖像的中心點
+    # center = (1966, 54)
+    center = (2969,2429)
+    # 取得旋轉矩陣
+    M = cv2.getRotationMatrix2D(center, index*60, 1.0)
+    cos=np.abs(M[0,0])
+    sin=np.abs(M[0,1])
+    nw = int((h*sin)+(w*cos)) #往右移，需要更大空間
+    nh = int((h*cos)+(w*sin))
+    
+     
+    # 旋轉圖像
+    output = cv2.warpAffine(img, M, (nw, nh))
+    output_Image.append(Image.fromarray(np.uint8(output)))
 
+for i in range(6):    
+    r,g,b,a = output_Image[i].split() 
+    bg.paste(output_Image[i],(1500, 1500),mask=a)   
 
+    # 3 取得旋轉矩陣
+    # M = cv2.getRotationMatrix2D(center, -60, 1.0)
+    # cos=np.abs(M[0,0])
+    # sin=np.abs(M[0,1])
+    # nw = int((h*sin)+(w*cos))
+    # nh = int((h*cos)+(w*sin))
 
-img = blackToClear(img)
-img2 = blackToClear(img2)
-
-cv2.namedWindow('image22', cv2.WINDOW_KEEPRATIO)
-cv2.imshow('image22',img2)
-cv2.setMouseCallback('image22', show_xy)
-bg = Image.new('RGBA',(8000, 8000), '#FFFFFF')
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-
-# 1 
-output_Image = Image.fromarray(np.uint8(img))
-r,g,b,a = output_Image.split()
-bg.paste(output_Image,(1500, 1500),mask=a)
- 
-
-# 2取得圖像的高度和寬度
-(h, w) = img.shape[:2]
-# 計算圖像的中心點
-# center = (1966, 54)
-center = (2969,2429)
-# 取得旋轉矩陣
-M = cv2.getRotationMatrix2D(center, 60, 1.0)
-cos=np.abs(M[0,0])
-sin=np.abs(M[0,1])
-nw = int((h*sin)+(w*cos)+1000) #往右移，需要更大空間
-nh = int((h*cos)+(w*sin))
- 
-# 旋轉圖像
-output = cv2.warpAffine(img, M, (nw, nh))
-output_Image = Image.fromarray(np.uint8(output))
-r,g,b,a = output_Image.split() 
-bg.paste(output_Image,(1500, 1500),mask=a)
-
-# 3 取得旋轉矩陣
-M = cv2.getRotationMatrix2D(center, -60, 1.0)
-cos=np.abs(M[0,0])
-sin=np.abs(M[0,1])
-nw = int((h*sin)+(w*cos))
-nh = int((h*cos)+(w*sin))
-
-output2 = cv2.warpAffine(img2, M, (6000, 6000))
-output_Image2 = Image.fromarray(np.uint8(output2))
-r,g,b,a = output_Image2.split()
-bg.paste(output_Image2,(1500, 1500),mask=a)
+# output2 = cv2.warpAffine(img2, M, (6000, 6000))
+# output_Image2 = Image.fromarray(np.uint8(output2))
+# r,g,b,a = output_Image2.split()
+# bg.paste(output_Image2,(1500, 1500),mask=a)
 
 #轉換為cv2格式
 bg = cv2.cvtColor(np.asarray(bg), cv2.COLOR_RGBA2BGRA)
 cv2.namedWindow('image', cv2.WINDOW_KEEPRATIO)
-cv2.imshow('image',bg)
-            
-# cv2.imwrite('oxxostudio23.png', img)    # 存檔儲存為 png，不可用imshow
+cv2.imshow('image',bg)            
+# cv2.imwrite('connect_output.png', bg)    # 存檔儲存為 png，不可用imshow
 cv2.waitKey(0)
 cv2.destroyAllWindows()
