defeed8166991ab24bdeea2a001a4dc1ba74958a
Ivy60627 <40078512+Ivy60627@users.noreply.github.com>
2024-01-30 19:00:56 +0800

    0130 sum the areas for each label

--- convert_RLE_to_maskarea.py
+++ convert_RLE_to_maskarea.py
@@ -10,6 +10,33 @@ newdict={}
 json_list=[]
 maskarea=[]
 
+def add_masks(labels, mask):
+
+    def find_indices(labels: list, value: int) -> list:
+        """
+        Get the index list of the specific elements
+        :labels: The label list
+        :value: The element that will get the indexs in labels list
+        :Return: return the list of indexs
+
+        example:
+        labels = [1, 2, 2, 3, 2, 3]
+        value = 2
+        return: [1, 2, 4]
+
+        """
+
+        return [index for index, element in enumerate(labels) if element == value]
+
+    label_element_index_list = [find_indices(labels, x) for x in set(labels)]
+
+    new_mask = []
+    for index in label_element_index_list:
+        element_masks_sum = sum([mask[x] for x in index])
+        new_mask.append(element_masks_sum)
+
+    return list(set(labels)), new_mask
+
 for root,dirs,files in os.walk(json_file_path):
 	for file in files:
 		json_list.append(file)
@@ -28,13 +55,17 @@ for json_file in json_list:
     for i, _ in enumerate(data["labels"]):
         mask_decode = mask_util.decode(data["masks"][i])
         maskarea.append(np.count_nonzero(mask_decode))
-        
-    newdict["labels"]=data['labels']
-    newdict["maskarea"]=maskarea
+ 
     
+    newdict["labels"], newdict["maskarea"]=add_masks(data["labels"], maskarea)
+    
+
+
+        
     json_out_file = str(json_out_file_path + json_file)
     tf = open(json_out_file, "w")
     json.dump(newdict,tf)
     tf.close()
     print(f"Convert {json_file} successfully.")
+    maskarea=[]
 
