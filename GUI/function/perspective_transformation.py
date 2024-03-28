82bd64a01e9bbbe2c3433df3061bf43524c42794
Ivy60627 <40078512+Ivy60627@users.noreply.github.com>
2024-03-28 17:19:35 +0800

    20240328 restructure other function

--- GUI/function/perspective_transformation.py
+++ GUI/function/perspective_transformation.py
@@ -1,39 +1,20 @@
-import os
 import cv2
 import numpy as np
 
-
-def show_xy(event, x, y, flags, userdata):
-    if (event != 0):
-        print(event, x, y, flags)
+from function.helper_function import read_image_list
 
 
-class PerspectiveTransformation():
-    def __init__(self, path: str):
-        # Set the path of the pictures
-        self.path = path
-        self.pic_list = []
+def transformation_image(pic_path: str, output_path: str) -> np.ndarray:
+    # Define the transformation location of the image
+    x_1 = 250
+    p1 = np.float32([[500, x_1], [500, 3000 - x_1], [3550, 0], [3550, 3000]])
+    x_2 = 15
+    p2 = np.float32([[0, 0], [0, 3000], [4000, x_2], [4000, 3000 - x_2]])
 
-        # Define the transformation location of the image      
-        self.x_1 = 250
-        self.p1 = np.float32([[500, self.x_1], [500, 3000 - self.x_1], [3550, 0], [3550, 3000]])
-        self.x_2 = 15
-        self.p2 = np.float32([[0, 0], [0, 3000], [4000, self.x_2], [4000, 3000 - self.x_2]])
-
-        # Get the transformation matrix from p1 to p2
-        self.m = cv2.getPerspectiveTransform(self.p1, self.p2)
-
-    def read_image(self):
-        for root, dirs, files in os.walk(self.path):
-            for file in files:
-                file = file.rstrip(".png")
-                self.pic_list.append(file)
-        return self.pic_list
-
-    def transformation_image(self, pic_list):
-        for pic in pic_list[:6]:
-            self.img = cv2.imread(self.path + pic + '.png', )
-            self.output = cv2.warpPerspective(self.img, self.m, (4000, 4000))
-            self.img_original = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
-            cv2.imwrite('images\\transformation\\' + pic + '.png', self.output)
-        return self.output
+    # Get the transformation matrix from p1 to p2
+    m = cv2.getPerspectiveTransform(p1, p2)
+    for pic in read_image_list(pic_path)[:6]:
+        img = cv2.imread(pic_path + pic + '.png')
+        output = cv2.warpPerspective(img, m, (4000, 4000))
+        cv2.imwrite(f'{output_path}{pic}.png', output)
+    return output
